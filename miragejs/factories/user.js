80c8eea4d4266e5a00eeabdaa346326cfdd00289
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-11-12 10:50:06 -0300

    feat: add miragejs to project and configure it

---
+++ miragejs/factories/user.js
@@ -0,0 +1,19 @@
+import { Factory } from 'miragejs';
+import { faker } from '@faker-js/faker';
+import { randomNumber } from './utils';
+
+export default {
+  user: Factory.extend({
+    name() {
+      return faker.fake('{{name.findName}}');
+    },
+    mobile() {
+      return faker.fake('{{phone.phoneNumber}}');
+    },
+    afterCreate(user, server) {
+      const messages = server.createList('message', randomNumber(10), { user });
+
+      user.update({ messages });
+    },
+  }),
+};
